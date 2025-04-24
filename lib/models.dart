1a462c9b9c3a6b94a0f4b325d4ff8f879a163407
Julian Schmitt <juschmitt91@gmail.com>
2025-04-24 08:12:41 +0200

    refactor: Convert all models to use freezed

--- lib/models.dart
+++ lib/models.dart
@@ -6,111 +6,48 @@ enum Direction { Up, Down, Idle }
 
 enum PassengerStatus { Waiting, InElevator, Arrived, Disappeared }
 
-class Passenger {
-  final String id;
-  final int spawnFloor;
-  final int destinationFloor;
-  PassengerStatus status;
-  DateTime spawnTime; // To calculate wait time
+@freezed
+abstract class Passenger with _$Passenger {
+  const factory Passenger({
+    required String id,
+    required int spawnFloor,
+    required int destinationFloor,
+    @Default(PassengerStatus.Waiting) PassengerStatus status,
+    required DateTime spawnTime,
+  }) = _Passenger;
 
-  Passenger({
-    required this.id,
-    required this.spawnFloor,
-    required this.destinationFloor,
-    this.status = PassengerStatus.Waiting,
-    required this.spawnTime,
-  });
-  Passenger copyWith({
-    String? id,
-    int? spawnFloor,
-    int? destinationFloor,
-    PassengerStatus? status,
-    DateTime? spawnTime,
-  }) {
-    return Passenger(
-      id: id ?? this.id,
-      spawnFloor: spawnFloor ?? this.spawnFloor,
-      destinationFloor: destinationFloor ?? this.destinationFloor,
-      status: status ?? this.status,
-      spawnTime: spawnTime ?? this.spawnTime,
-    );
-  }
+  const Passenger._();
 
   Direction get direction =>
       destinationFloor > spawnFloor ? Direction.Up : Direction.Down;
 }
 
-class Elevator {
-  final String id;
-  final double currentFloor;
-  final Direction direction;
-  final List<int> destinationQueue;
-  final List<Passenger> passengers;
-  final int capacity;
-  final bool isSelected;
+@freezed
+abstract class Elevator with _$Elevator {
+  const factory Elevator({
+    required String id,
+    required double currentFloor,
+    required Direction direction,
+    required List<int> destinationQueue,
+    required List<Passenger> passengers,
+    required int capacity,
+    @Default(false) bool isSelected,
+  }) = _Elevator;
 
-  const Elevator({
-    required this.id,
-    required this.currentFloor,
-    required this.direction,
-    required this.destinationQueue,
-    required this.passengers,
-    required this.capacity,
-    this.isSelected = false,
-  });
-
-  Elevator copyWith({
-    String? id,
-    double? currentFloor,
-    Direction? direction,
-    List<int>? destinationQueue,
-    List<Passenger>? passengers,
-    int? capacity,
-    bool? isSelected,
-  }) {
-    return Elevator(
-      id: id ?? this.id,
-      currentFloor: currentFloor ?? this.currentFloor,
-      direction: direction ?? this.direction,
-      destinationQueue: destinationQueue ?? this.destinationQueue,
-      passengers: passengers ?? this.passengers,
-      capacity: capacity ?? this.capacity,
-      isSelected: isSelected ?? this.isSelected,
-    );
-  }
+  const Elevator._();
 
   bool get isFull => passengers.length >= capacity;
 }
 
-class Floor {
-  final int floorNumber;
-  List<Passenger> waitingUp;
-  List<Passenger> waitingDown;
-  bool callUpLit;
-  bool callDownLit;
-
-  Floor({
-    required this.floorNumber,
-    this.waitingUp = const [],
-    this.waitingDown = const [],
-    this.callUpLit = false,
-    this.callDownLit = false,
-  }); //
-  Floor copyWith({
-    int? floorNumber,
-    List<Passenger>? waitingUp,
-    List<Passenger>? waitingDown,
-    bool? callUpLit,
-    bool? callDownLit,
-  }) {
-    return Floor(
-      floorNumber: floorNumber ?? this.floorNumber,
-      waitingUp: waitingUp ?? this.waitingUp,
-      waitingDown: waitingDown ?? this.waitingDown,
-      callUpLit: callUpLit ?? this.callUpLit,
-      callDownLit: callDownLit ?? this.callDownLit,
-    );
-  }
+@freezed
+abstract class Floor with _$Floor {
+  const factory Floor({
+    required int floorNumber,
+    @Default([]) List<Passenger> waitingUp,
+    @Default([]) List<Passenger> waitingDown,
+    @Default(false) bool callUpLit,
+    @Default(false) bool callDownLit,
+  }) = _Floor;
 }
 
 @freezed
--- lib/models.freezed.dart
+++ lib/models.freezed.dart
@@ -13,6 +13,453 @@ part of 'models.dart';
 // dart format off
 T _$identity<T>(T value) => value;
 /// @nodoc
+mixin _$Passenger {
+
+ String get id; int get spawnFloor; int get destinationFloor; PassengerStatus get status; DateTime get spawnTime;
+/// Create a copy of Passenger
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$PassengerCopyWith<Passenger> get copyWith => _$PassengerCopyWithImpl<Passenger>(this as Passenger, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Passenger&&(identical(other.id, id) || other.id == id)&&(identical(other.spawnFloor, spawnFloor) || other.spawnFloor == spawnFloor)&&(identical(other.destinationFloor, destinationFloor) || other.destinationFloor == destinationFloor)&&(identical(other.status, status) || other.status == status)&&(identical(other.spawnTime, spawnTime) || other.spawnTime == spawnTime));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,spawnFloor,destinationFloor,status,spawnTime);
+
+@override
+String toString() {
+  return 'Passenger(id: $id, spawnFloor: $spawnFloor, destinationFloor: $destinationFloor, status: $status, spawnTime: $spawnTime)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $PassengerCopyWith<$Res>  {
+  factory $PassengerCopyWith(Passenger value, $Res Function(Passenger) _then) = _$PassengerCopyWithImpl;
+@useResult
+$Res call({
+ String id, int spawnFloor, int destinationFloor, PassengerStatus status, DateTime spawnTime
+});
+
+
+
+
+}
+/// @nodoc
+class _$PassengerCopyWithImpl<$Res>
+    implements $PassengerCopyWith<$Res> {
+  _$PassengerCopyWithImpl(this._self, this._then);
+
+  final Passenger _self;
+  final $Res Function(Passenger) _then;
+
+/// Create a copy of Passenger
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? spawnFloor = null,Object? destinationFloor = null,Object? status = null,Object? spawnTime = null,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,spawnFloor: null == spawnFloor ? _self.spawnFloor : spawnFloor // ignore: cast_nullable_to_non_nullable
+as int,destinationFloor: null == destinationFloor ? _self.destinationFloor : destinationFloor // ignore: cast_nullable_to_non_nullable
+as int,status: null == status ? _self.status : status // ignore: cast_nullable_to_non_nullable
+as PassengerStatus,spawnTime: null == spawnTime ? _self.spawnTime : spawnTime // ignore: cast_nullable_to_non_nullable
+as DateTime,
+  ));
+}
+
+}
+
+
+/// @nodoc
+
+
+class _Passenger extends Passenger {
+  const _Passenger({required this.id, required this.spawnFloor, required this.destinationFloor, this.status = PassengerStatus.Waiting, required this.spawnTime}): super._();
+  
+
+@override final  String id;
+@override final  int spawnFloor;
+@override final  int destinationFloor;
+@override@JsonKey() final  PassengerStatus status;
+@override final  DateTime spawnTime;
+
+/// Create a copy of Passenger
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$PassengerCopyWith<_Passenger> get copyWith => __$PassengerCopyWithImpl<_Passenger>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Passenger&&(identical(other.id, id) || other.id == id)&&(identical(other.spawnFloor, spawnFloor) || other.spawnFloor == spawnFloor)&&(identical(other.destinationFloor, destinationFloor) || other.destinationFloor == destinationFloor)&&(identical(other.status, status) || other.status == status)&&(identical(other.spawnTime, spawnTime) || other.spawnTime == spawnTime));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,spawnFloor,destinationFloor,status,spawnTime);
+
+@override
+String toString() {
+  return 'Passenger(id: $id, spawnFloor: $spawnFloor, destinationFloor: $destinationFloor, status: $status, spawnTime: $spawnTime)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$PassengerCopyWith<$Res> implements $PassengerCopyWith<$Res> {
+  factory _$PassengerCopyWith(_Passenger value, $Res Function(_Passenger) _then) = __$PassengerCopyWithImpl;
+@override @useResult
+$Res call({
+ String id, int spawnFloor, int destinationFloor, PassengerStatus status, DateTime spawnTime
+});
+
+
+
+
+}
+/// @nodoc
+class __$PassengerCopyWithImpl<$Res>
+    implements _$PassengerCopyWith<$Res> {
+  __$PassengerCopyWithImpl(this._self, this._then);
+
+  final _Passenger _self;
+  final $Res Function(_Passenger) _then;
+
+/// Create a copy of Passenger
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? spawnFloor = null,Object? destinationFloor = null,Object? status = null,Object? spawnTime = null,}) {
+  return _then(_Passenger(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,spawnFloor: null == spawnFloor ? _self.spawnFloor : spawnFloor // ignore: cast_nullable_to_non_nullable
+as int,destinationFloor: null == destinationFloor ? _self.destinationFloor : destinationFloor // ignore: cast_nullable_to_non_nullable
+as int,status: null == status ? _self.status : status // ignore: cast_nullable_to_non_nullable
+as PassengerStatus,spawnTime: null == spawnTime ? _self.spawnTime : spawnTime // ignore: cast_nullable_to_non_nullable
+as DateTime,
+  ));
+}
+
+
+}
+
+/// @nodoc
+mixin _$Elevator {
+
+ String get id; double get currentFloor; Direction get direction; List<int> get destinationQueue; List<Passenger> get passengers; int get capacity; bool get isSelected;
+/// Create a copy of Elevator
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$ElevatorCopyWith<Elevator> get copyWith => _$ElevatorCopyWithImpl<Elevator>(this as Elevator, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Elevator&&(identical(other.id, id) || other.id == id)&&(identical(other.currentFloor, currentFloor) || other.currentFloor == currentFloor)&&(identical(other.direction, direction) || other.direction == direction)&&const DeepCollectionEquality().equals(other.destinationQueue, destinationQueue)&&const DeepCollectionEquality().equals(other.passengers, passengers)&&(identical(other.capacity, capacity) || other.capacity == capacity)&&(identical(other.isSelected, isSelected) || other.isSelected == isSelected));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,currentFloor,direction,const DeepCollectionEquality().hash(destinationQueue),const DeepCollectionEquality().hash(passengers),capacity,isSelected);
+
+@override
+String toString() {
+  return 'Elevator(id: $id, currentFloor: $currentFloor, direction: $direction, destinationQueue: $destinationQueue, passengers: $passengers, capacity: $capacity, isSelected: $isSelected)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $ElevatorCopyWith<$Res>  {
+  factory $ElevatorCopyWith(Elevator value, $Res Function(Elevator) _then) = _$ElevatorCopyWithImpl;
+@useResult
+$Res call({
+ String id, double currentFloor, Direction direction, List<int> destinationQueue, List<Passenger> passengers, int capacity, bool isSelected
+});
+
+
+
+
+}
+/// @nodoc
+class _$ElevatorCopyWithImpl<$Res>
+    implements $ElevatorCopyWith<$Res> {
+  _$ElevatorCopyWithImpl(this._self, this._then);
+
+  final Elevator _self;
+  final $Res Function(Elevator) _then;
+
+/// Create a copy of Elevator
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? currentFloor = null,Object? direction = null,Object? destinationQueue = null,Object? passengers = null,Object? capacity = null,Object? isSelected = null,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,currentFloor: null == currentFloor ? _self.currentFloor : currentFloor // ignore: cast_nullable_to_non_nullable
+as double,direction: null == direction ? _self.direction : direction // ignore: cast_nullable_to_non_nullable
+as Direction,destinationQueue: null == destinationQueue ? _self.destinationQueue : destinationQueue // ignore: cast_nullable_to_non_nullable
+as List<int>,passengers: null == passengers ? _self.passengers : passengers // ignore: cast_nullable_to_non_nullable
+as List<Passenger>,capacity: null == capacity ? _self.capacity : capacity // ignore: cast_nullable_to_non_nullable
+as int,isSelected: null == isSelected ? _self.isSelected : isSelected // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+}
+
+
+/// @nodoc
+
+
+class _Elevator extends Elevator {
+  const _Elevator({required this.id, required this.currentFloor, required this.direction, required final  List<int> destinationQueue, required final  List<Passenger> passengers, required this.capacity, this.isSelected = false}): _destinationQueue = destinationQueue,_passengers = passengers,super._();
+  
+
+@override final  String id;
+@override final  double currentFloor;
+@override final  Direction direction;
+ final  List<int> _destinationQueue;
+@override List<int> get destinationQueue {
+  if (_destinationQueue is EqualUnmodifiableListView) return _destinationQueue;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_destinationQueue);
+}
+
+ final  List<Passenger> _passengers;
+@override List<Passenger> get passengers {
+  if (_passengers is EqualUnmodifiableListView) return _passengers;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_passengers);
+}
+
+@override final  int capacity;
+@override@JsonKey() final  bool isSelected;
+
+/// Create a copy of Elevator
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$ElevatorCopyWith<_Elevator> get copyWith => __$ElevatorCopyWithImpl<_Elevator>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Elevator&&(identical(other.id, id) || other.id == id)&&(identical(other.currentFloor, currentFloor) || other.currentFloor == currentFloor)&&(identical(other.direction, direction) || other.direction == direction)&&const DeepCollectionEquality().equals(other._destinationQueue, _destinationQueue)&&const DeepCollectionEquality().equals(other._passengers, _passengers)&&(identical(other.capacity, capacity) || other.capacity == capacity)&&(identical(other.isSelected, isSelected) || other.isSelected == isSelected));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,id,currentFloor,direction,const DeepCollectionEquality().hash(_destinationQueue),const DeepCollectionEquality().hash(_passengers),capacity,isSelected);
+
+@override
+String toString() {
+  return 'Elevator(id: $id, currentFloor: $currentFloor, direction: $direction, destinationQueue: $destinationQueue, passengers: $passengers, capacity: $capacity, isSelected: $isSelected)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$ElevatorCopyWith<$Res> implements $ElevatorCopyWith<$Res> {
+  factory _$ElevatorCopyWith(_Elevator value, $Res Function(_Elevator) _then) = __$ElevatorCopyWithImpl;
+@override @useResult
+$Res call({
+ String id, double currentFloor, Direction direction, List<int> destinationQueue, List<Passenger> passengers, int capacity, bool isSelected
+});
+
+
+
+
+}
+/// @nodoc
+class __$ElevatorCopyWithImpl<$Res>
+    implements _$ElevatorCopyWith<$Res> {
+  __$ElevatorCopyWithImpl(this._self, this._then);
+
+  final _Elevator _self;
+  final $Res Function(_Elevator) _then;
+
+/// Create a copy of Elevator
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? currentFloor = null,Object? direction = null,Object? destinationQueue = null,Object? passengers = null,Object? capacity = null,Object? isSelected = null,}) {
+  return _then(_Elevator(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,currentFloor: null == currentFloor ? _self.currentFloor : currentFloor // ignore: cast_nullable_to_non_nullable
+as double,direction: null == direction ? _self.direction : direction // ignore: cast_nullable_to_non_nullable
+as Direction,destinationQueue: null == destinationQueue ? _self._destinationQueue : destinationQueue // ignore: cast_nullable_to_non_nullable
+as List<int>,passengers: null == passengers ? _self._passengers : passengers // ignore: cast_nullable_to_non_nullable
+as List<Passenger>,capacity: null == capacity ? _self.capacity : capacity // ignore: cast_nullable_to_non_nullable
+as int,isSelected: null == isSelected ? _self.isSelected : isSelected // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+
+}
+
+/// @nodoc
+mixin _$Floor {
+
+ int get floorNumber; List<Passenger> get waitingUp; List<Passenger> get waitingDown; bool get callUpLit; bool get callDownLit;
+/// Create a copy of Floor
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$FloorCopyWith<Floor> get copyWith => _$FloorCopyWithImpl<Floor>(this as Floor, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Floor&&(identical(other.floorNumber, floorNumber) || other.floorNumber == floorNumber)&&const DeepCollectionEquality().equals(other.waitingUp, waitingUp)&&const DeepCollectionEquality().equals(other.waitingDown, waitingDown)&&(identical(other.callUpLit, callUpLit) || other.callUpLit == callUpLit)&&(identical(other.callDownLit, callDownLit) || other.callDownLit == callDownLit));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,floorNumber,const DeepCollectionEquality().hash(waitingUp),const DeepCollectionEquality().hash(waitingDown),callUpLit,callDownLit);
+
+@override
+String toString() {
+  return 'Floor(floorNumber: $floorNumber, waitingUp: $waitingUp, waitingDown: $waitingDown, callUpLit: $callUpLit, callDownLit: $callDownLit)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $FloorCopyWith<$Res>  {
+  factory $FloorCopyWith(Floor value, $Res Function(Floor) _then) = _$FloorCopyWithImpl;
+@useResult
+$Res call({
+ int floorNumber, List<Passenger> waitingUp, List<Passenger> waitingDown, bool callUpLit, bool callDownLit
+});
+
+
+
+
+}
+/// @nodoc
+class _$FloorCopyWithImpl<$Res>
+    implements $FloorCopyWith<$Res> {
+  _$FloorCopyWithImpl(this._self, this._then);
+
+  final Floor _self;
+  final $Res Function(Floor) _then;
+
+/// Create a copy of Floor
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? floorNumber = null,Object? waitingUp = null,Object? waitingDown = null,Object? callUpLit = null,Object? callDownLit = null,}) {
+  return _then(_self.copyWith(
+floorNumber: null == floorNumber ? _self.floorNumber : floorNumber // ignore: cast_nullable_to_non_nullable
+as int,waitingUp: null == waitingUp ? _self.waitingUp : waitingUp // ignore: cast_nullable_to_non_nullable
+as List<Passenger>,waitingDown: null == waitingDown ? _self.waitingDown : waitingDown // ignore: cast_nullable_to_non_nullable
+as List<Passenger>,callUpLit: null == callUpLit ? _self.callUpLit : callUpLit // ignore: cast_nullable_to_non_nullable
+as bool,callDownLit: null == callDownLit ? _self.callDownLit : callDownLit // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+}
+
+
+/// @nodoc
+
+
+class _Floor implements Floor {
+  const _Floor({required this.floorNumber, final  List<Passenger> waitingUp = const [], final  List<Passenger> waitingDown = const [], this.callUpLit = false, this.callDownLit = false}): _waitingUp = waitingUp,_waitingDown = waitingDown;
+  
+
+@override final  int floorNumber;
+ final  List<Passenger> _waitingUp;
+@override@JsonKey() List<Passenger> get waitingUp {
+  if (_waitingUp is EqualUnmodifiableListView) return _waitingUp;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_waitingUp);
+}
+
+ final  List<Passenger> _waitingDown;
+@override@JsonKey() List<Passenger> get waitingDown {
+  if (_waitingDown is EqualUnmodifiableListView) return _waitingDown;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(_waitingDown);
+}
+
+@override@JsonKey() final  bool callUpLit;
+@override@JsonKey() final  bool callDownLit;
+
+/// Create a copy of Floor
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$FloorCopyWith<_Floor> get copyWith => __$FloorCopyWithImpl<_Floor>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Floor&&(identical(other.floorNumber, floorNumber) || other.floorNumber == floorNumber)&&const DeepCollectionEquality().equals(other._waitingUp, _waitingUp)&&const DeepCollectionEquality().equals(other._waitingDown, _waitingDown)&&(identical(other.callUpLit, callUpLit) || other.callUpLit == callUpLit)&&(identical(other.callDownLit, callDownLit) || other.callDownLit == callDownLit));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,floorNumber,const DeepCollectionEquality().hash(_waitingUp),const DeepCollectionEquality().hash(_waitingDown),callUpLit,callDownLit);
+
+@override
+String toString() {
+  return 'Floor(floorNumber: $floorNumber, waitingUp: $waitingUp, waitingDown: $waitingDown, callUpLit: $callUpLit, callDownLit: $callDownLit)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$FloorCopyWith<$Res> implements $FloorCopyWith<$Res> {
+  factory _$FloorCopyWith(_Floor value, $Res Function(_Floor) _then) = __$FloorCopyWithImpl;
+@override @useResult
+$Res call({
+ int floorNumber, List<Passenger> waitingUp, List<Passenger> waitingDown, bool callUpLit, bool callDownLit
+});
+
+
+
+
+}
+/// @nodoc
+class __$FloorCopyWithImpl<$Res>
+    implements _$FloorCopyWith<$Res> {
+  __$FloorCopyWithImpl(this._self, this._then);
+
+  final _Floor _self;
+  final $Res Function(_Floor) _then;
+
+/// Create a copy of Floor
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? floorNumber = null,Object? waitingUp = null,Object? waitingDown = null,Object? callUpLit = null,Object? callDownLit = null,}) {
+  return _then(_Floor(
+floorNumber: null == floorNumber ? _self.floorNumber : floorNumber // ignore: cast_nullable_to_non_nullable
+as int,waitingUp: null == waitingUp ? _self._waitingUp : waitingUp // ignore: cast_nullable_to_non_nullable
+as List<Passenger>,waitingDown: null == waitingDown ? _self._waitingDown : waitingDown // ignore: cast_nullable_to_non_nullable
+as List<Passenger>,callUpLit: null == callUpLit ? _self.callUpLit : callUpLit // ignore: cast_nullable_to_non_nullable
+as bool,callDownLit: null == callDownLit ? _self.callDownLit : callDownLit // ignore: cast_nullable_to_non_nullable
+as bool,
+  ));
+}
+
+
+}
+
+/// @nodoc
 mixin _$GameState {
 
  List<Elevator> get elevators; List<Floor> get floors; List<Passenger> get allPassengers; int get score; bool get isGameOver; Duration get elapsedGameTime;
